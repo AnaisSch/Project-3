ac98b4f05e6a8eb8e0b1bf590a6709466ae267ef
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-20 14:44:40 +0100

    ajout de app.use dossier public

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
+
 app.listen(3000, () => {
-    console.log("SERVER STARTED...");
+    console.log("LE SERVEUR DÉMARRE...");
 });
+
+app.use(express.static('./public'));
